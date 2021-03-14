2041928e8843400fa993f31bad2878cee3ad8463
wst <wst@LAPTOP-1970ATAV>
2021-03-14 23:21:07 +0100

    zweite änderung

--- myTestApp/myTestApp/Program.cs
+++ myTestApp/myTestApp/Program.cs
@@ -8,6 +8,7 @@ namespace myTestApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World2!");
+            Console.WriteLine("Hello World3!");
         }
     }
 }
