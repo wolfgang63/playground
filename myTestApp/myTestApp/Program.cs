d33f6c515165227bd2cb6637a6401bd7fbeb7010
wst <wst@LAPTOP-1970ATAV>
2021-03-14 23:10:04 +0100

    init

---
+++ myTestApp/myTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
